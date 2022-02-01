8199216001e50ba2897903d81f4137a037ab6ac4
Steve Canneaux <seamlessloops@gmail.com>
2022-02-01 18:35:01 +0000

    Add files via upload

---
+++ use of importing specific code from module.py
@@ -0,0 +1,8 @@
+from ShapeAreas import areaRect
+
+result = areaRect(4, 8) - areaRect(2, 3)
+print("The area of the room =", result, "\n")
+print ("or\n\n")
+result = areaRect(4, 5) + areaRect(2, 3)
+print("The area of the room =", result, "\n")
+
